108763596195461bb1147816fdda238a9a61122e
Mahlckov <tiphaine.chapeau97@orange.fr>
2023-02-02 16:50:58 +0100

    Retrait modifié avec Articles noArticle

--- src/main/java/fr/eni/javaee/eniencheres/bo/Retrait.java
+++ src/main/java/fr/eni/javaee/eniencheres/bo/Retrait.java
@@ -1,17 +1,27 @@
 package fr.eni.javaee.eniencheres.bo;
 
 public class Retrait {
-	 private String rue ;
+	 private  Articles noArticle;
+	 private  String rue ;
 	 private  String code_postal;
 	 private  String ville ;
 	
 	 
-	 public Retrait(String rue, String code_postal, String ville) {
+	 public Retrait(Articles noArticle, String rue, String code_postal, String ville) {
 		super();
+		this.noArticle =noArticle;
 		this.rue = rue;
 		this.code_postal = code_postal;
 		this.ville = ville;
 	}
+	 
+	 public Retrait(String rue, String code_postal, String ville) {
+			super();
+			this.rue = rue;
+			this.code_postal = code_postal;
+			this.ville = ville;
+	}
+		 
 	 
 	public String getRue() {
 		return rue;
@@ -31,7 +41,12 @@ public class Retrait {
 	public void setVille(String ville) {
 		this.ville = ville;
 	}
-	 
-	 
-	 
+
+	public Articles getNoArticle() {
+		return noArticle;
+	}
+
+	public void setNoArticle(Articles noArticle) {
+		this.noArticle = noArticle;
+	}	 
 }
